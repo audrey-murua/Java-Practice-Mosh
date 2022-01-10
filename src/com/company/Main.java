f93ecaf3089971dc91751e8118efbd01fbe93dc2
audrey murua <audrey.murua1@gmail.com>
2022-01-09 21:40:14 -0600

    first java program with mosh

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+        //System is a class
+        //out is a field
+        //println is a method
+        //the parenthesis is where you can enter value, in this case a string of "Hello World"
+    }
+}
