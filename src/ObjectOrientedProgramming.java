5ce1749f1f8d1751f33176865502e6bc5308536e
audrey murua <audrey.murua1@gmail.com>
2022-01-14 00:46:02 -0600

    comparison operators overview

---
+++ src/ObjectOrientedProgramming.java
@@ -0,0 +1,35 @@
+public class ObjectOrientedProgramming {
+
+    public static void main(String[] args) {
+
+        //COMPARISON OPERATORS
+
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y);
+        // = is an assignment operator, assigns a value to a variable
+        // == is an equality operator, it compares two values
+        // x == y is a boolean expression, is the value of x equal to the value of y, true or false
+
+        int e = 1;
+        int h = 1;
+        System.out.println(e != h);
+        //!= is the inequality operator (means not equal to)
+
+        int o = 1;
+        int u = 2;
+        System.out.println(o <= u);
+        //<= less than or equal to operator
+
+        int v = 1;
+        int w = 2;
+        System.out.println(v >= w);
+        //>= greater than or equal to operator
+
+
+
+
+
+
+    }
+}
